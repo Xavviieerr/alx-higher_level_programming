c170205c0345a77c59f2488be0749278c80e3039
Xavviieerr <ogbupaul545@gmail.com>
2024-04-07 14:14:15 -0700

    task seven

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+num = 0
 for x in range(0, 9):
-    for y in range(1, 10):
+    num += 1
+    for y in range(num, 10):
         if x == 8 and y == 9:
             print("{}".format(89))
         else:
