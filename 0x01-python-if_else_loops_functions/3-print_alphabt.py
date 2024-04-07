aafda2178d71107a13dd4ffa8c618faf6cfc53cc
Xavviieerr <ogbupaul545@gmail.com>
2024-04-07 13:02:41 -0700

    task four

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,5 +3,5 @@ letter = ""
 for x in range(97, 123):
     if x != 113:
         if x != 101:
-             letter += "{}".format(chr(x))
+            letter += "{}".format(chr(x))
 print(letter, end="")
