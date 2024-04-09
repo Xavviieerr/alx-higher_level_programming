d2eedbf3223df8d582e57153c0c3c467143be1c1
Xavviieerr <ogbupaul545@gmail.com>
2024-04-09 09:08:37 -0700

    task two

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+list = sys.argv
+len_list = len(list) - 1
+if __name__ == '__main__':
+    if len_list == 0:
+        print("{} arguments.".format(len_list))
+    if len_list >= 1:
+        print("{} arguments :".format(len_list))
+        for x in range(1, len_list + 1):
+            print("{} {}".format(x, list[x]))
