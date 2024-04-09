5be6938ff0e7e47ac3bf75c6abdc63c189e00595
Xavviieerr <ogbupaul545@gmail.com>
2024-04-09 09:11:22 -0700

    task two

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     if len_list == 0:
         print("{} arguments.".format(len_list))
     if len_list >= 1:
-        print("{} arguments :".format(len_list))
+        print("{} arguments:".format(len_list))
         for x in range(1, len_list + 1):
             print("{}: {}".format(x, list[x]))
