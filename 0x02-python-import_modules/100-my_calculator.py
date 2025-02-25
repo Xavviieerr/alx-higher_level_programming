9379bec9d82a57bfa0baa416e43a114a35bfd12d
Xavviieerr <ogbupaul545@gmail.com>
2025-02-25 06:34:34 -0800

    fixed my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ from sys import argv
 from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
     len_arg = len(argv)
-    if len_arg != 3:
+    if len_arg != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit (1)
     if argv[2] not in ["+", "-", "*", "/"]:
@@ -11,7 +11,7 @@ if __name__ == '__main__':
         exit (1)
     else:
         a = int(argv[1])
-        b = int(argv[2])
+        b = int(argv[3])
         if argv[2] == "+":
             sum = add(a, b)
             print("{} {} {} = {}".format(a, argv[2], b, sum))
