df53f17a528374302235dcdd04e07c1255a7756f
Xavviieerr <ogbupaul545@gmail.com>
2025-02-25 23:31:38 -0800

    fixed matrix printer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix == [[]]:
-        print("")
+        print("".format())
     for row in matrix:
         if len(row) == 3:
-            print("{} {} {}".format(row[0], row[1], row[2]))
+            print("{:d} {:d} {:d}".format(row[0], row[1], row[2]))
         elif len(row) == 2:
-            print("{} {}".format(row[0], row[1]))
+            print("{:d} {:d}".format(row[0], row[1]))
         elif len(row) == 1:
-            print("{}".format(row[0]))
+            print("{:d}".format(row[0]))
