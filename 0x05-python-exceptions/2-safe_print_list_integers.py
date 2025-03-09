d84d8d8befc4d91179ef925b26557590a771f052
Xavviieerr <ogbupaul545@gmail.com>
2025-03-09 00:05:46 -0800

    fixed function for printing safe integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        new_list = list(map(lambda item: item, my_list))
-        length = 0
-        count = 0
-        for w in new_list:
-            length += 1
-        if x <= length:
-            for y in range(0, x):
+    new_list = list(map(lambda item: item, my_list))
+    length = 0
+    count = 0
+    for w in new_list:
+        length += 1
+    if x <= length:
+        for y in range(0, x):
+            try:
                 if isinstance(new_list[y], int):
                     count += 1
                     print("{:d}".format(new_list[y]), end="")
-            print()
-        else:
-            raise IndexError("Index out of range")
+            except IndexError:
+                print()
+                raise IndexError("Index out of range")
+        print()
         return count
-    except IndexError as e:
-        print(f"Error: {e}")
-
