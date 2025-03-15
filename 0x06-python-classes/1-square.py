113a785edd319332f0579c8896eb859e605ef469
Xavviieerr <ogbupaul545@gmail.com>
2025-03-15 06:58:27 -0700

    createdfile with first initialized variable

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""this is class Square module."""
+class Square:
+    """this class defines attributes."""
+    def __init__(self, size):
+        """initializes sizea attribute"""
+        self.__size = size
