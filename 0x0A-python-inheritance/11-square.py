64bced9a5a7dd69012c849d92ddc910feebee7b4
Xavviieerr <ogbupaul545@gmail.com>
2025-03-28 17:11:12 -0700

    twelve

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""main file"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """main class"""
+
+    def __init__(self, size):
+        """initialization"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """claculates"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """returns string"""
+        return f"[Square] {self.__size}/{self.__size}"
