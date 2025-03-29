f0b59150ebced899a8009da86ac6d5075c460a57
Xavviieerr <ogbupaul545@gmail.com>
2025-03-29 02:28:27 -0700

    zero

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""main file"""
+"""Function to read a text file and print its content"""
+
 def read_file(filename=""):
-    """reads afile"""
+    """Reads a UTF-8 encoded file and prints its contents"""
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
