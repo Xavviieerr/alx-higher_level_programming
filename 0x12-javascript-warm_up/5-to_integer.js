06c6214b826771673f3a5cbfffe5a20007129ce4
Xavviieerr <ogbupaul545@gmail.com>
2025-04-01 08:57:55 -0700

    five

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+if (parseInt(argv[2]) === 'NaN') {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${parseInt(argv[2])}`);
+}
